aa481f567e077601b9040c36ce29f9e3f09587a7
wilberquito <wbq.software@gmail.com>
2023-04-14 09:39:52 +0200

    Updates set configurations

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -5,3 +5,30 @@
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+--
+
+local opt = vim.opt
+
+-- highlight current line
+opt.cursorline = true
+
+-- Numbers
+opt.number = true
+opt.relativenumber = true
+
+-- ding ding!
+opt.errorbells = false
+
+-- don't wrap long lines
+opt.wrap = false
+
+ -- scroll offwidth so it's not the very bottom
+opt.scrolloff = 6
+
+-- highlight yanked text for 200ms using the "Visual" highlight group
+vim.cmd[[
+    augroup highlight_yank
+    autocmd!
+    au TextYankPost * silent! lua vim.highlight.on_yank({higroup="Visual", timeout=200})
+    augroup END
+]]
