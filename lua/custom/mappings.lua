5be426d065188c89c0b53e6eea992016bfac3b0f
wilberquito <wbq.software@gmail.com>
2023-07-28 22:26:40 +0200

    Change default configuration

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -12,6 +12,33 @@ local talkativeopts = {
   noremap = true
 }
 
+-- Disable nvim integrated term mapping
+M.disabled = {
+
+  t = {
+    -- toggle in terminal mode
+    ["<A-i>"] = "",
+
+    ["<A-h>"] = "",
+
+    ["<A-v>"] = "",
+  },
+
+  n = {
+    -- toggle in normal mode
+    ["<A-i>"] = "",
+
+    ["<A-h>"] = "",
+
+    ["<A-v>"] = "",
+
+    -- new
+    ["<leader>h"] = "",
+
+    ["<leader>v"] = ""
+  }
+}
+
 
 M.general = {
   n = {
