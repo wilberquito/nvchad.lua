fddf0c8144997047c1f1f841fb395b9e9f52d1f1
wilberquito <wbq.software@gmail.com>
2023-08-26 16:59:51 +0200

    drop unused configuration

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,12 +1,3 @@
--- local autocmd = vim.api.nvim_create_autocmd
-
--- Auto resize panes when resizing nvim window
--- autocmd("VimResized", {
---   pattern = "*",
---   command = "tabdo wincmd =",
--- })
---
-
 local opt = vim.opt
 
 -- highlight current line
